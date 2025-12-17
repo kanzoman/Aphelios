289dc804e855d4d94a9c5f64fd7c3ec484fe8c6a
kanzoman <kanzo4business@gmail.com>
2025-12-17 23:19:49 +0100

    wa3r

--- script.js
+++ script.js
@@ -51,9 +51,9 @@ function loadProducts() {
   container.innerHTML = ""; // clear
 
 const products = [
-  { id: 1, name: "THORNED BAT HOODIE", price: "250 MAD", img: "https://i.ibb.co/dJ47zJNP/Gemini-Generated-Image-kcsoa4kcsoa4kcso.png", sold: false },
+  { id: 1, name: "THORNED BAT HOODIE", price: "250 MAD", img: "https://i.ibb.co/dJ47zJNP/Gemini-Generated-Image-kcsoa4kcsoa4kcso.png", sold: true },
   { id: 2, name: "DRIPPING CASTLE HOODIE", price: "250 MAD", img: "https://i.ibb.co/HLKxCDBm/Gemini-Generated-Image-qj4vugqj4vugqj4v.png", sold: false },
-  { id: 3, name: "BAT THORN SLEEVE HOODIE", price: "250 MAD", img: "https://i.postimg.cc/6QHzPh6V/Gemini-Generated-Image-vrz34cvrz34cvrz3.png", sold: false },
+  { id: 3, name: "BAT THORN SLEEVE HOODIE", price: "250 MAD", img: "https://i.postimg.cc/6QHzPh6V/Gemini-Generated-Image-vrz34cvrz34cvrz3.png", sold: true },
   { id: 4, name: "WHITE RAVEN CROSS HOODIE", price: "220 MAD", img: "https://i.postimg.cc/vBVq6VMN/Design-sans-titre.png", sold: true },
 ];
 
